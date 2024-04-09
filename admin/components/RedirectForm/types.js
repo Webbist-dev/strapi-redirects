672d787ac5fa1301363e0e0930a242341a1ad1ae
Webbist-dev <info@alex-bennett.co.uk>
2024-04-09 19:52:46 +0100

    Fix: update the enum render to default to 302

--- admin/components/RedirectForm/types.js
+++ admin/components/RedirectForm/types.js
@@ -1,6 +1,6 @@
 const redirectTypeOptions = [
-  "moved_permanently_301",
   "found_302",
+  "moved_permanently_301",
   "temporary_redirect_307",
   "gone_410",
   "unavailable_for_legal_reasons_451"
