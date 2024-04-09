38ec00d20dc40d9416ac990b7cf5145be8951fec
Derrick Mehaffy <derrickmehaffy@gmail.com>
2024-04-09 11:23:45 -0700

    Fix multiple issues with webbist

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,19 +1,15 @@
-'use strict';
+"use strict";
 
-const contentType = require('./content-type');
-const redirects = require('./redirects');
+const contentType = require("./content-type");
+const redirects = require("./redirects");
 
 module.exports = {
-  'content-api': {
-    type: 'content-api',
-    routes: [
-      ...contentType
-    ]
+  "content-api": {
+    type: "content-api",
+    routes: [...contentType],
   },
-  'admin': {
-    type: 'admin',
-    routes: [
-      ...redirects
-    ]
-  }
+  admin: {
+    type: "admin",
+    routes: [...redirects],
+  },
 };
--- server/routes/redirects.js
+++ server/routes/redirects.js
@@ -1,40 +1,40 @@
-'use strict';
+"use strict";
 
 module.exports = [
   {
-    method: 'GET',
-    path: '/:id',
-    handler: 'redirects.findOne',
-    config: { policies: [] }
+    method: "GET",
+    path: "/:id",
+    handler: "redirects.findOne",
+    config: { policies: [] },
   },
   {
-    method: 'GET',
-    path: '/',
-    handler: 'redirects.findAll',
-    config: { policies: [], auth: false }
+    method: "GET",
+    path: "/",
+    handler: "redirects.findAll",
+    config: { policies: [] },
   },
   {
-    method: 'POST',
-    path: '/',
-    handler: 'redirects.create',
-    config: { policies: [] }
+    method: "POST",
+    path: "/",
+    handler: "redirects.create",
+    config: { policies: [] },
   },
   {
-    method: 'PUT',
-    path: '/:id',
-    handler: 'redirects.update',
-    config: { policies: [] }
+    method: "PUT",
+    path: "/:id",
+    handler: "redirects.update",
+    config: { policies: [] },
   },
   {
-    method: 'DELETE',
-    path: '/:id',
-    handler: 'redirects.delete',
-    config: { policies: [] }
+    method: "DELETE",
+    path: "/:id",
+    handler: "redirects.delete",
+    config: { policies: [] },
   },
   {
-    method: 'POST',
-    path: '/import',
-    handler: 'redirects.import',
-    config: { policies: [] }
-  }
+    method: "POST",
+    path: "/import",
+    handler: "redirects.import",
+    config: { policies: [] },
+  },
 ];
